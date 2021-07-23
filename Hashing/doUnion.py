429cbbf08fb69c5be1ce0e13a379e5aa319a9ed0
Ankit Kumar <44130267+Ankygurjar@users.noreply.github.com>
2021-07-23 16:07:31 -0400

    Create doUnion.py

---
+++ Hashing/doUnion.py
@@ -0,0 +1,34 @@
+
+
+
+
+
+
+
+
+
+
+
+def doUnion(self,a,n,b,m):
+        
+        #code here
+        newSet = set()
+        for i in range(len(a)):
+            newSet.add(a[i])
+        for i in range(len(b)):
+            newSet.add(b[i])
+        
+        return len(newSet)
+
+
+
+
+
+
+
+
+
+
+
+
+
