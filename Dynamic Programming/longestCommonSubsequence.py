fa882ae87085af065eef4fe44fdd3cb0fcea7b8e
Ankit Kumar <44130267+Ankygurjar@users.noreply.github.com>
2021-08-01 13:05:47 -0400

    Update longestCommonSubsequence.py

--- Dynamic Programming/longestCommonSubsequence.py
+++ Dynamic Programming/longestCommonSubsequence.py
@@ -7,17 +7,16 @@
 
 
 
-
-def lcs(self,x,y,s1,s2):
-    n, m = len(s1), len(s2)
-    dp = [[0]*(m+1) for z in range(n+1)]
-    for i in range(n):
-        for j in range(m):
-            if s1[i] == s2[j]:
-                dp[i+1][j+1] = dp[i][j]+1
+def lcs(s1, s2):
+    m, n = len(s1), len(s2)
+    dp = [[0 for _ in range(n + 1)] for y in range(m + 1)]
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if s1[i - 1] == s2[j - 1]:
+                dp[i][j] = 1 + dp[i - 1][j - 1]
             else:
-                dp[i+1][j+1] = max(dp[i][j+1], dp[i+1][j])
-    return dp[-1][-1]
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+    return dp[m][n]
 
 
 
