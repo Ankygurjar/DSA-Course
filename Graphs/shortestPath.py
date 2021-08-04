406beda75eacaa5eba1a230e4cc76e0ec80d193b
Ankit Kumar <44130267+Ankygurjar@users.noreply.github.com>
2021-08-04 14:41:49 -0400

    Update shortestPath.py

--- Graphs/shortestPath.py
+++ Graphs/shortestPath.py
@@ -1,30 +1,4 @@
-
-
-
-
-
-
 from collections import deque
-
-
-class Graph:
-    root = dict()
-
-    def nodes(self, nodes: list):
-        for node in nodes:
-            if node not in self.root.keys():
-                self.root[node] = []
-
-    def vertices(self, node, edges: list):
-        if node in self.root.keys():
-            cur: list = self.root.get(node)
-            for edge in edges:
-                if edge not in cur:
-                    cur.append(edge)
-
-
-# WRITE CODE HERE
-
 
 def shortestPath(graph: dict, s: int):
     visited, dist = {}, {}
@@ -46,16 +20,6 @@ def shortestPath(graph: dict, s: int):
     print(dist)
 
 
-# ENDS HERE
-
-g = Graph()
-
-g.nodes([0, 1, 2, 3])
-g.vertices(0, [1, 2])
-g.vertices(1, [2, 3])
-g.vertices(2, [1, 3, 0])
-g.vertices(3, [1, 2])
-myList: list = g.root.values()
 shortestPath(g.root, 3)
 
 
